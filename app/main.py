7545cf7cedd6c862d6c832a0cb0f2b3efca7aaaf
Yoann Bonnaventure <yo.bonnaventure@gmail.com>
2023-02-08 16:30:27 +0100

    ajout de commentaires

--- app/main.py
+++ app/main.py
@@ -1,6 +1,41 @@
 '''
 Jeu Puissance4
 '''
+import tkinter as tk
 
-def grille_valide():
+global hauteur
+global largeur
+hauteur = []
+largeur = []
+
+def verifier(i,j):
     
+    try :
+        hauteur[i]
+        largeur[j]
+    except :
+        print("cette position va être créée")
+        hauteur.append(i)
+        largeur.append(j)
+
+    print(i,j)
+
+def button_click(i, j):
+    print(str(i) + str(j))
+    verifier(i,j)
+
+root = tk.Tk()
+
+#Création de l'UI
+def grid(hauteur,largeur):
+    for i in range(hauteur):
+        for j in range(largeur):
+            b = tk.Button(root, text="Button " + str(i) + str(j), width=10, height=5, command=lambda i=i, j=j: button_click(i, j))
+            b.grid(row=i, column=j)
+
+    root.mainloop()
+    
+grid(7,6)
+
+        
+
--- test/tests.py
+++ test/tests.py
@@ -7,4 +7,4 @@ def test_unitaire_1():
     assert(grille_valide()) == True
 
 def test_unitaire_2():
-    assert(jouer("A","2C")) == True
+    assert(verifier(4,2)) == True
