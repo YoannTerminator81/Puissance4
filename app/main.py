3a41f344df86c499bb746b4c587c16a4c6629530
Kyllian Liegeois <kyllian.liegeois@e.rascol.net>
2023-02-08 15:26:00 +0100

    jb j j

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-'''
-Jeu Puissance4
-'''
+import tkinter as tk
 
-def grille_valide():
+def grid(hauteur,largeur):
+    root = tk.Tk()
+
+    for i in range(hauteur):
+        for j in range(largeur):
+            b = tk.Button(root, text="Button " + str(i) + str(j), width=10, height=5)
+            b.grid(row=i, column=j)
+
+    root.mainloop()
+
+grid(7,10)
     
