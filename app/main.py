71b3fbe29115809e57cf05805fdf0daae4c41b46
Kyllian Liegeois <kyllian.liegeois@e.rascol.net>
2023-02-08 15:38:41 +0100

    grille

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 
-def grid(hauteur,largeur):
-    root = tk.Tk()
+def button_click(i, j):
+    print(str(i) + str(j))
 
+root = tk.Tk()
+
+def grid(hauteur,largeur):
     for i in range(hauteur):
         for j in range(largeur):
-            b = tk.Button(root, text="Button " + str(i) + str(j), width=10, height=5)
+            b = tk.Button(root, text="Button " + str(i) + str(j), width=10, height=5, command=lambda i=i, j=j: button_click(i, j))
             b.grid(row=i, column=j)
 
     root.mainloop()
-
-grid(7,10)
     
+grid(9,6)
